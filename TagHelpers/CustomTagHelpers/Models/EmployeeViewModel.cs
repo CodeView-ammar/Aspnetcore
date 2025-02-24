ff1a7dbb95711809262a6f961d50ec5413d8e35f
eng.ammar abdul wadood <64986292+CodeView-ammar@users.noreply.github.com>
2025-02-24 15:06:31 +0300

    Delete TagHelpers directory

--- TagHelpers/CustomTagHelpers/Models/EmployeeViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomTagHelper.Models
-{
-    using System.ComponentModel.DataAnnotations;
-    public class EmployeeViewModel
-    {
-        public string Name { get; set; }
-        public string Designation { get; set; }
-    }
-}
